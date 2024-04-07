154653961c5c90c2400bdde17de63379f6ad2be1
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-04-07 22:59:17 +0200

    reset GameBox

--- src/main/scala/de/htwg/se/minesweeper/model/gameComponent/gameBaseImpl/GameBox.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/gameComponent/gameBaseImpl/GameBox.scala
@@ -1,16 +1,7 @@
 package de.htwg.se.minesweeper.model.gameComponent.gameBaseImpl
 
-case class GameBox(game: Option[Game] = None) {
-    def apply(f: Game => Game) = copy(game = game.map(f))
 
-    def insertBomb(newBombs: Int) = apply(_.insertBomb(newBombs))
-    def insertSide(newSide: Int) = apply(_.insertSide(newSide))
-    def insertTime(newTime: Int) = apply(_.insertTime(newTime))
-    def insertBoard(newBoard: String) = apply(_.insertBoard(newBoard))
-}
-
-end GameBox
-/* case class GameBox(game: Option[Game]):
+case class GameBox(game: Option[Game]):
 
     def insertBomb(newBombs: Int): GameBox = game match
         case Some(game: Game) => copy(Some(game.insertBomb(newBombs)))
@@ -27,5 +18,15 @@ end GameBox
     def insertBoard(newBoard: String): GameBox = game match
         case Some(game: Game) => copy(Some(game.insertBoard(newBoard)))
         case None => copy(None)
+
+end GameBox
+/* case class GameBox(game: Option[Game] = None) {
+    def apply(f: Game => Game) = copy(game = game.map(f))
+
+    def insertBomb(newBombs: Int) = apply(_.insertBomb(newBombs))
+    def insertSide(newSide: Int) = apply(_.insertSide(newSide))
+    def insertTime(newTime: Int) = apply(_.insertTime(newTime))
+    def insertBoard(newBoard: String) = apply(_.insertBoard(newBoard))
+}
 
 end GameBox */
