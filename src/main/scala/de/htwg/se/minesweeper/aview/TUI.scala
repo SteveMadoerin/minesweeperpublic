a4faa182f0941acf1fb930798b7a2b71278cd979
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-04-08 14:21:39 +0200

    cleaned TUI

--- src/main/scala/de/htwg/se/minesweeper/aview/TUI.scala
+++ src/main/scala/de/htwg/se/minesweeper/aview/TUI.scala
@@ -2,7 +2,7 @@ package de.htwg.se.minesweeper.aview
 
 
 import de.htwg.se.minesweeper.controller.controllerComponent.IController
-import de.htwg.se.minesweeper.util.{Observer, Move, Event, MoveBox}
+import de.htwg.se.minesweeper.util.{Observer, Move, Event}
 
 import scala.io.StdIn.readLine
 import scala.util.{Try, Success, Failure}
@@ -28,7 +28,7 @@ class TUI(using var controller: IController) extends Observer:
             case Event.Exit => System.exit(0); false
             case _ => false
     
-    def userInX(rawInput: String): MoveBox = {
+    def userInX(rawInput: String): Option[Move] = {
         val cleanInputPattern: Regex = """^[a-z]{1}[0-9]{4}$""".r
         val onlyOneStringPattern: Regex = """^[q|h|r|z|y|o|f|s|l|u|t]{1}$""".r
 
@@ -38,14 +38,14 @@ class TUI(using var controller: IController) extends Observer:
             case _ => infoMessages(">> Invalid Input Format");"e"
         
         input match
-            case "q" => System.exit(0); MoveBox(None)
-            case "h" => controller.helpMenu; MoveBox(None)
-            case "r" => controller.cheat; MoveBox(None)
-            case "z" => controller.makeAndPublish(controller.undo); MoveBox(None)
-            case "y" => controller.makeAndPublish(controller.redo); MoveBox(None)
-            case "s" => controller.saveGame; MoveBox(None)
-            case "l" => controller.loadGame; MoveBox(None)
-            case "e" => MoveBox(None)
+            case "q" => System.exit(0); None
+            case "h" => controller.helpMenu; None
+            case "r" => controller.cheat; None
+            case "z" => controller.makeAndPublish(controller.undo); None
+            case "y" => controller.makeAndPublish(controller.redo); None
+            case "s" => controller.saveGame; None
+            case "l" => controller.loadGame; None
+            case "e" => None
             case _ => {
                 val charAccumulator = input.toCharArray()
                 
@@ -64,7 +64,7 @@ class TUI(using var controller: IController) extends Observer:
                     case Some(i) => {if controller.game.side > i._1 && controller.game.side > i._2 then Some(Move(action, i._1, i._2)) else { infoMessages(">> Invalid Move: Coordinates out of bounds"); None}} // no var game
                     case None => None
                 }
-                MoveBox(validCoordinates) // validCoordinates
+                validCoordinates
             }
         
     }
@@ -73,8 +73,8 @@ class TUI(using var controller: IController) extends Observer:
     def parseInputandPrintLoop(firstMoveCheck: Boolean): Unit = {
         infoMessages("Enter your move (<action><x><y>, eg. o0102, q to quit, h for help):")
         val stillFirstMove = userInX(readLine) match {
-            case MoveBox(None) => firstMoveCheck
-            case MoveBox(Some(move)) =>
+            case None => firstMoveCheck
+            case Some(move) =>
                 processMove(move, firstMoveCheck)
                 false
         }
--- src/main/scala/de/htwg/se/minesweeper/util/MoveBox.scala
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.minesweeper.util
-
-case class MoveBox(move: Option[Move]):
-    
-    def validate: MoveBox = move match
-        case Some(move: Move) => copy(Some(move.validate))
-        case None => copy(None)
-
-end MoveBox
