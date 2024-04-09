f4d4ed27fa36d5899f1528a45b1b1abc220129c2
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-04-09 08:20:03 +0200

    clean Move

--- src/main/scala/de/htwg/se/minesweeper/util/Move.scala
+++ src/main/scala/de/htwg/se/minesweeper/util/Move.scala
@@ -1,9 +1,3 @@
 package de.htwg.se.minesweeper.util
 
-case class Move(value: String, x: Int, y: Int):
-
-    def validate: Move = value match
-        case "open" => copy("open", x, y)
-        case "flag" => copy("flag", x, y)
-        case "unflag" => copy("unflag", x, y)
-        case _ => copy("invalid", x, y)
+case class Move(value: String, x: Int, y: Int)
