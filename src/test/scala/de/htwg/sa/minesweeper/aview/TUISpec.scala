cd4b5bb0da9d71e72e28013eee3db9745e2967d7
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-04-11 10:56:07 +0200

    tui spec change

--- src/test/scala/de/htwg/sa/minesweeper/aview/TUISpec.scala
+++ src/test/scala/de/htwg/sa/minesweeper/aview/TUISpec.scala
@@ -4,7 +4,7 @@ package de.htwg.sa.minesweeper.aview
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
 import de.htwg.sa.minesweeper.controller.controllerComponent.controllerBaseImpl.Controller
-import de.htwg.se.minesweeper.model.gameComponent.gameBaseImpl._
+import de.htwg.sa.minesweeper.model.gameComponent.gameBaseImpl._
 import de.htwg.sa.minesweeper.util.Move
 import de.htwg.sa.minesweeper.util.Event
 import de.htwg.sa.minesweeper.Default.{given}
