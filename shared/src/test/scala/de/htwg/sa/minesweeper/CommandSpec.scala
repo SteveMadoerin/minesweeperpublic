a9801949b84026db044a8a281228f1df824dc288
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-04-15 15:28:05 +0200

    added tests to Module shared

---
+++ shared/src/test/scala/de/htwg/sa/minesweeper/CommandSpec.scala
@@ -0,0 +1,35 @@
+package de.htwg.sa.minesweeper.shared
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.sa.minesweeper.shared.Command
+
+class testCommand extends  Command[Int]:
+    override def noStep(t: Int): Int = t
+    override def doStep(t: Int): Int = t + 1
+    override def undoStep(t: Int): Int = t - 1
+    override def redoStep(t: Int): Int = t + 1
+
+class CommandSpec extends AnyWordSpec{
+    "Command" should{
+        val command = new testCommand
+
+        "have no step" in{
+            command.noStep(1) should be(1)
+        }
+
+        "have do step" in{
+            command.doStep(1) should be(2)
+        }
+
+        "have undo step" in{
+            command.undoStep(1) should be(0)
+        }
+
+        "have redo step" in{
+            command.redoStep(1) should be(2)
+        }
+    }
+
+
+}
---
+++ shared/src/test/scala/de/htwg/sa/minesweeper/ObserverSpec.scala
@@ -0,0 +1,54 @@
+package de.htwg.sa.minesweeper.shared
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.sa.minesweeper.shared.{Event, Observer, Observable}
+
+class ObserverSpec extends AnyWordSpec{
+
+    "The Observable" should{
+        val observable = new Observable {}
+
+        "have no subscribers" in {
+            assert(observable.subscribers.isEmpty)
+        }
+
+        "allow an Observer to be added" in {
+            val observer = new Observer{
+                override def update(e: Event): Boolean = false
+            }
+            observable.add(observer)
+            assert(observable.subscribers.contains(observer))
+        }
+
+        "allow an Observer to be removed" in {
+            val observer = new Observer{
+                override def update(e: Event): Boolean = false
+            }
+            observable.add(observer)
+            observable.remove(observer)
+            assert(!observable.subscribers.contains(observer))
+        }
+
+        "notify its observers when an update occurs" in {
+            var notified = false
+            val observer = new Observer{
+                override def update(e: Event) = 
+                {
+                    notified = true
+                    true
+                }
+            }
+            observable.add(observer)
+            observable.notifyObservers(Event.Next)
+            assert(notified)
+        }
+
+
+
+    }
+
+
+
+
+}
